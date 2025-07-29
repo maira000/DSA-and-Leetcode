d52784c53fe6452648c6618eb9d00496e820f023
maira000 <150447045+maira000@users.noreply.github.com>
2025-07-29 11:47:14 +0500

    Time: 3 ms (88.75%) | Memory: 13.6 MB (62.51%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums):
+        insert_pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[insert_pos] = nums[i]
+                insert_pos += 1
+        for i in range(insert_pos, len(nums)):
+            nums[i] = 0
