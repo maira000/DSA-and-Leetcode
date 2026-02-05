c14429e460416c20bfcb9111d33ca3b4d9aba003
Maira Naveed <150447045+maira000@users.noreply.github.com>
2026-02-05 15:26:02 +0500

    Time: 11 ms (52.53%) | Memory: 13.5 MB (76.66%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        map_s_to_t = {}
+        map_t_to_s = {}
+        
+        for i in range(len(s)):
+            char_s = s[i]
+            char_t = t[i]
+            
+            if char_s in map_s_to_t:
+                if map_s_to_t[char_s] != char_t:
+                    return False
+            else:
+                map_s_to_t[char_s] = char_t
+            
+            if char_t in map_t_to_s:
+                if map_t_to_s[char_t] != char_s:
+                    return False
+            else:
+                map_t_to_s[char_t] = char_s
+        
+        return True
