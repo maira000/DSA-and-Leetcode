83112474bd36d029396fa6e2a0e4aac771b74e9f
Maira Naveed <150447045+maira000@users.noreply.github.com>
2026-02-23 22:40:16 +0500

    Time: 8 ms (61.04%) | Memory: 12.7 MB (66.55%) - LeetSync

---
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        diff1 = list(set1 - set2)
+        diff2 = list(set2 - set1)
+
+        return[diff1,diff2]
